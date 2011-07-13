335441b6ecb22f2d6bd9b5d960117954f14f8fe6
Sebastian Slomski <sebastian@simple-systems.org>
2011-07-13 14:22:29 +0200

    animation done via css3

--- js/app.coffee
+++ js/app.coffee
@@ -1,32 +1,40 @@
 $ ->
+    $('div.box:first').addClass('selected')
+
     $('div.box').click (e) ->
+        # duration in seconds
+        duration = 0.3
+
         $this = $(@)
         selected = $this.parent().find('.selected')
 
         if selected.get(0) is @
             return
 
-        $this.parent().append $('<div id="animation"></div>')
-
         position = selected.position()
+        # Don't click so fast
         if selected.height()
             position.height = selected.height()
 
-        $('div#animation').css position
+        $('div#animation').remove()
+        $this.parent().append $('<div id="animation"></div>')
+        $('div#animation').css(position)
 
-        selected.removeClass 'selected'
+        transition =
+            'height': "#{$this.height()}px !important"
+            'top': "#{$this.position().top}px !important"
 
-        $('div#animation').animate(
-          {
-            left: $this.position().left,
-            top: $this.position().top,
-            height: $this.height()
-          },
-          {
-              duration: 300,
-              complete: ->
-                  selected.removeClass 'selected'
-                  $this.addClass 'selected'
-                  $('div#animation').remove()
-          }
-        )
+        for prefix in ['-moz-', '-webkit-', '']
+            transition["#{prefix}transition-property"] = 'height, top'
+            transition["#{prefix}transition-duration"] = "#{duration}s"
+
+        selected.removeClass('selected')
+        $('div#animation').css(transition)
+
+        done = ->
+            $('div#animation').remove()
+            # When you click faster than the duration, several boxes might be selected
+            $('div.box').removeClass('selected')
+            $this.addClass('selected')
+
+        setTimeout(done, duration*1000)
--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
 (function() {
   $(function() {
+    $('div.box:first').addClass('selected');
     return $('div.box').click(function(e) {
-      var $this, position, selected;
+      var $this, done, duration, position, prefix, selected, transition, _i, _len, _ref;
+      duration = 0.3;
       $this = $(this);
       selected = $this.parent().find('.selected');
       if (selected.get(0) === this) {
         return;
       }
-      $this.parent().append($('<div id="animation"></div>'));
       position = selected.position();
       if (selected.height()) {
         position.height = selected.height();
       }
+      $('div#animation').remove();
+      $this.parent().append($('<div id="animation"></div>'));
       $('div#animation').css(position);
+      transition = {
+        'height': "" + ($this.height()) + "px !important",
+        'top': "" + ($this.position().top) + "px !important"
+      };
+      _ref = ['-moz-', '-webkit-', ''];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        prefix = _ref[_i];
+        transition["" + prefix + "transition-property"] = 'height, top';
+        transition["" + prefix + "transition-duration"] = "" + duration + "s";
+      }
       selected.removeClass('selected');
-      return $('div#animation').animate({
-        left: $this.position().left,
-        top: $this.position().top,
-        height: $this.height()
-      }, {
-        duration: 300,
-        complete: function() {
-          selected.removeClass('selected');
-          $this.addClass('selected');
-          return $('div#animation').remove();
-        }
-      });
+      $('div#animation').css(transition);
+      done = function() {
+        $('div#animation').remove();
+        $('div.box').removeClass('selected');
+        return $this.addClass('selected');
+      };
+      return setTimeout(done, duration * 1000);
     });
   });
 }).call(this);
